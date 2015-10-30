08aa781b70c655945985982ae5837f2ef4227db3
Jorge Centeno Fernández <jcenteno@macbook.local>
2015-10-30 23:26:08 +0100

    SentenceService proposal

---
+++ src/main/java/demo/domain/Sentence.java
@@ -0,0 +1,25 @@
+package demo.domain;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+import demo.domain.SentenceWord.Role;
+
+public class Sentence {
+
+	// ordered by role so to make easier print the sentence
+	private Map<Role,String> words = new TreeMap<>(); 
+	
+	public void add(SentenceWord word){
+		words.put(word.getRole(),word.getWord());
+	}
+	
+	@Override
+	public String toString() {
+	  StringBuilder sb = new StringBuilder();
+	  for (Role role: words.keySet()){
+		  sb.append(words.get(role)).append(' ');
+	  }
+	  return sb.toString();
+	}
+}
